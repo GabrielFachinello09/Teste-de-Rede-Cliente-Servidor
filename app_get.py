b19f5accbb0099c520394f26662d8314a3b323fc
GabrielFachinello09 <gabrielfachinello134@gmail.com>
2026-03-19 10:07:08 -0300

    Servidor Back-end- Rede cliente-servidor

---
+++ app_get.py
@@ -0,0 +1,30 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+
+class Servidor(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Welcome to my server")
+
+
+    def do_POST(self): 
+
+        tamanho = int(self.headers['Content-Length'])
+
+        dados = self.rfile.read(tamanho)
+
+        print("Dados recebidos:", dados.decode())
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"POST RECEBIDO")
+
+
+HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever()
+
+
+    
+    
+    
+    
