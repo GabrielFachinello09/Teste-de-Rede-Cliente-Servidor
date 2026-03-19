899292edc92f39e30c9258888132dc16cace45b2
GabrielFachinello09 <gabrielfachinello134@gmail.com>
2026-03-19 10:52:30 -0300

    comentários de linhas de código

--- app_get.py
+++ app_get.py
@@ -1,30 +1,35 @@
-from http.server import HTTPServer,BaseHTTPRequestHandler
+from http.server import HTTPServer,BaseHTTPRequestHandler 
+#Importa duas classes da biblioteca:
+#HTTPServer: cria o servidor HTTP.
+#BaseHTTPRequestHandler: classe base para lidar com requisições (GET, POST.)
 
 class Servidor(BaseHTTPRequestHandler):
+#Cria classe chamada servidor herdando o (BaseHTTPRequestHandler
 
     def do_GET(self):
-        self.send_response(200)
+        self.send_response(200) 
+#Envia o código HTTP 200, que significa “OK” (requisição bem-sucedida).
         self.end_headers()
         self.wfile.write(b"Welcome to my server")
+#Envia o conteúdo da resposta para o cliente.
 
 
     def do_POST(self): 
+#Chamado quando o cliente envia dados via POST.
 
-        tamanho = int(self.headers['Content-Length'])
+        tamanho = int(self.headers['Content-Length']) #identifica o tamanho dos arquivos enviados na requisição.
 
-        dados = self.rfile.read(tamanho)
+        dados = self.rfile.read(tamanho) #lê os dados enviados.
 
-        print("Dados recebidos:", dados.decode())
-
-        self.send_response(200)
+        print("Dados recebidos:", dados.decode()) 
+        self.send_response(200) 
         self.end_headers()
-        self.wfile.write(b"POST RECEBIDO")
+        self.wfile.write(b"POST RECEBIDO") 
 
 
 HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever()
-print("Rede Cliente-Servidor")
+print("teste")
 
-    
     
     
     
